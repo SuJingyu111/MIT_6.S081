12252e1bb9fe2af769ffa4053ae62bd8c2c52bf9
Jingyu Su <gar123su@gmail.com>
2023-01-07 23:11:07 -0800

    lab1 pingpong update

--- user/pingpong.c
+++ user/pingpong.c
@@ -11,9 +11,7 @@
 int
 main(int argc, char *argv[]) {
     int p[2];
-    //printf("here\n");
     pipe(p);
-    //printf("created pipe\n");
     if (fork() == 0) {
         char readBuff[10];
         if (read(p[0], readBuff, 1) < 0) {
@@ -25,23 +23,17 @@ main(int argc, char *argv[]) {
             printf("child cannot write\n");
             exit(1);
         }
-        //printf("%d: after write\n", getpid());
         close(p[1]);
         exit(0);
     } else {
-        //printf("parent: 1\n");
         if (write(p[1], "c", 1) < 0) {
             printf("child cannot write\n");
             exit(1);
         }
         close(p[1]);
-        //printf("parent wrote to pipe\n");
         char readBuff[10];
         read(p[0], readBuff, 1);
         printf("%d: received pong\n", getpid());
         exit(0);
     }
-    //printf("%d: about to exit\n", getpid());
-    //exit(0);
-    //return 0;
 }
